6aa24dfd5060ef5aa05d238d2aa6b7ccd09b952c
malibuyukbudak <malibuyukbudak@gmail.com>
2021-06-22 16:42:03 +0300

    Main View classes created

---
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -0,0 +1,16 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView(){
+
+    }
+}
